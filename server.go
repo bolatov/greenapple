899605ef54b4fafdecbdf33996a3f059e8f33743
Almer Bolatov <almer.bolatov@gmail.com>
2016-03-14 14:39:16 +0600

    Add logging to file

--- server.go
+++ server.go
@@ -5,13 +5,22 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
-func main() {
-	router := NewRouter()
+func init() {
+	f, err := os.OpenFile("algo.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("Error opening file: %v", err)
+	}
+	defer f.Close()
+	log.SetOutput(f)
+}
 
+func main() {
 	portPtr := flag.String("port", "8080", "port number")
 	flag.Parse()
 
+	router := NewRouter()
 	log.Fatal(http.ListenAndServe(":"+(*portPtr), router))
 }
